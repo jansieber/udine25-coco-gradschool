a653152402c156b7172cf77ddc75a05967aa159c
Jan Sieber <j.sieber@exeter.ac.uk>
2025-12-15 16:58:56 +0100

    corrected typo in ab_demo

--- Bifurcations of equilibria/ab_demo.m
+++ Bifurcations of equilibria/ab_demo.m
@@ -4,9 +4,9 @@
 % parameters [alpha,beta,gamma]=reaction rate, exothermicity, cooling
 clear;
 format compact
+reaction=@(x,p)p(1,:).*(1-x(1,:)).*exp(x(2,:));
 ab=@(x,p)[...     % ODE r.h.s.
-    -x(1,:)+p(1,:).*(1-x(1,:)).*exp(x(2,:));...
-    -x(2,:)+p(2,:)*p(1,:).*(1-x(1,:)).*exp(x(2,:))-p(3,:).*x(2,:)];
+    -x(1,:)+reaction(x,p);...
+    -(1+p(3,:)).*x(2,:)+p(2,:).*reaction(x,p)];
 p0=[0;14;2];
 x0=[0;0];
-
